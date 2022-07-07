c1eb908e29551a5664c927bf6df4ca5d1ab580e8
William Hwang <willhwang90@gmail.com>
2022-07-07 11:30:25 -0400

    commit tree 5418ea44785af72a4d8980840e5f7d561bcd2442

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: %w(Fiction Non-Fiction)
+    validate :clickbait?
+
+    CLICKBAIT= [
+    "Won't Believe",
+    "Secret",
+    "Top",
+    "Guess"
+]
+    def clickbait?
+        if CLICKBAIT.none? { |pat| pat.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+    end
+
 end
